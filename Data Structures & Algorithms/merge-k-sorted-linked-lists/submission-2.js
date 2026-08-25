85aff9e225c1e55d4252b181e6f4e80b654fb928
Ritik <132301755+ritikvarun@users.noreply.github.com>
2026-08-25 21:21:32 +0530

    Add: merge-k-sorted-linked-lists - submission-2

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-2.js
@@ -0,0 +1,40 @@
+class Solution {
+    mergeKLists(lists) {
+        if (lists.length === 0) return null;
+
+        while (lists.length > 1) {
+            let mergedLists = [];
+
+            for (let i = 0; i < lists.length; i += 2) {
+                let l1 = lists[i];
+                let l2 = (i + 1 < lists.length) ? lists[i + 1] : null;
+
+                mergedLists.push(this.merge(l1, l2));
+            }
+
+            lists = mergedLists;
+        }
+
+        return lists[0];
+    }
+
+    merge(l1, l2) {
+        let dummy = new ListNode(0);
+        let tail = dummy;
+
+        while (l1 && l2) {
+            if (l1.val <= l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+
+        tail.next = l1 || l2;
+
+        return dummy.next;
+    }
+}
