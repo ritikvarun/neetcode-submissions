dd0aed21dcd29f9d1e342dd4f73e9ac138ec7957
Ritik <132301755+ritikvarun@users.noreply.github.com>
2026-09-06 23:05:53 +0530

    Add: level-order-traversal-of-binary-tree - submission-1

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-1.js
@@ -0,0 +1,26 @@
+class Solution {
+    levelOrder(root) {
+        if (root === null) return [];
+
+        let result = [];
+        let queue = [root];
+
+        while (queue.length > 0) {
+            let levelSize = queue.length;
+            let level = [];
+
+            for (let i = 0; i < levelSize; i++) {
+                let node = queue.shift();
+
+                level.push(node.val);
+
+                if (node.left !== null) queue.push(node.left);
+                if (node.right !== null) queue.push(node.right);
+            }
+
+            result.push(level);
+        }
+
+        return result;
+    }
+}
